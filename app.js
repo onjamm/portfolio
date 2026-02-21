c479d924f914181f228db281ab9abd7df790e11d
onjamm <75151498+onjamm@users.noreply.github.com>
2026-02-21 14:25:40 -0800

    added post route, admin route, confirmation page, etc.

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ import express from "express";
 const app = express();
 
 //Define the port number where our server will listen
-const PORT = 3000;
+//different than my others just in case we deploy on Digital Ocean
+const PORT = 3003;
 
-app.use(express.static("public"));
+//Middleware that allows express to read the form data and store it in the req.body
+app.use(express.urlencoded({ extended: true }));
 
 //Define a default "route" ('/)
 //req: contains information about the incoming request
@@ -15,6 +17,43 @@ app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.sendFile(`${import.meta.dirname}/views/contact.html`);
 });
+
+//Create a temp array to store the guestbook submissions
+const submissions = [];
+
+//Submit guesstbook route
+app.post("/submit-submission", (req, res) => {
+  const submission = {
+    fname: req.body.fname,
+    lname: req.body.lname,
+    email: req.body.email,
+    meet: req.body.meet,
+    html: req.body.html,
+    text: req.body.text,
+    mailingList: req.body.mailingList,
+    linkedIn: req.body.linkedIn,
+    formats: req.body.formats,
+    timestamp: new Date(),
+  };
+
+  //add submission object to submissions array
+  submissions.push(submission);
+
+  res.sendFile(`${import.meta.dirname}/views/confirmation.html`);
+});
+
+//admin route
+app.get("/admin", (req, res) => {
+  res.send(submissions);
+});
+
+//conrfirmation route
+app.get("/thank-you", (req, res) => {
+  res.sendFile(`${import.meta.dirname}/views/confirmation.html`);
+});
+
+//enable static file serving -- tells express where to look for static files
+app.use(express.static("public"));
 
 //Start the server and listen on the specified port
 app.listen(PORT, () => {
